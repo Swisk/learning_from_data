e35bb9a012fa34bf282efbef8ec17775c5d6dd99
Shane w <shane.w.s.k@gmail.com>
2020-02-16 07:53:34 +0800

    completed hw6

--- HW6.py
+++ HW6.py
@@ -17,18 +17,20 @@ Question 1
 Since H' is a subset of H, the approximation capability of H' should be worse. This implies that there will be more deterministic noise.
 '''
 print('Question 1')
-print('a')
+print('b')
 
 """
 Question 2
 
 """
 
+print('Question 2')
 #set up question
-df_train_raw = pd.read_csv(r'in.dta.txt', sep = '\s+', names=('x1', 'x2', 'y'))
-X = df_train_raw.loc['x1': 'x2']
-labels = df_train_raw['y']
+df_train_raw = pd.read_csv(r'c:/Users/shane/Documents/Tech Dev/learning_from_data/in.dta.txt', sep = '\s+', names=('x1', 'x2', 'y'))
+X = df_train_raw.loc[: , 'x1': 'x2']
+y = df_train_raw['y']
 
+X['1'] = 1
 X['x1^2'] = X['x1']**2
 X['x2^2'] = X['x2']**2
 X['x1x2'] = X['x1']*X['x2']
@@ -38,218 +40,140 @@ X['|x1+x2|'] = abs(X['x1'] + X['x2'])
 
 #building linear regression
 #one step learning
+xprimex = X.transpose().dot(X)
+inv = pd.DataFrame(np.linalg.pinv(xprimex.values), xprimex.columns, xprimex.index)
+
+weights = inv.dot(X.transpose()).dot(y)
+
+E_in = np.sum(y != np.sign(weights.dot(X.transpose())))/len(y)
+
+df_test = pd.read_csv(r'c:/Users/shane/Documents/Tech Dev/learning_from_data/out.dta.txt', sep = '\s+', names=('x1', 'x2', 'y'))
+X_test = df_test.loc[: , 'x1': 'x2']
+X_test['1'] = 1
+X_test['x1^2'] = X_test['x1']**2
+X_test['x2^2'] = X_test['x2']**2
+X_test['x1x2'] = X_test['x1']*X_test['x2']
+X_test['|x1-x2|'] = abs(X_test['x1'] - X_test['x2'])
+X_test['|x1+x2|'] = abs(X_test['x1'] + X_test['x2'])
+
+y_test = df_test['y']
+
+df_test['1'] = 1
 
 
+E_out = np.sum(y_test != np.sign(weights.dot(X_test.transpose())))/len(y_test)
+
+print(E_in, E_out)
+print('a')
 
 """
 Question 3
 
-The intuition is that the VC dimension is the number of free parameters. After transformation, we have more free parameters, so it is the case that the VC dimension can be as large as 15.
+Modify solution for weights to take into account the weight decay
 """
 
 print('Question 3')
-print('c')
+k = -3
+
+inv_decayed = pd.DataFrame(np.linalg.pinv(xprimex.values + 10**k * np.identity(8)), xprimex.columns, xprimex.index)
+weights_decayed = inv_decayed.dot(X.transpose()).dot(y)
+
+E_in = np.sum(y != np.sign(weights_decayed.dot(X.transpose())))/len(y)
+E_out = np.sum(y_test != np.sign(weights_decayed.dot(X_test.transpose())))/len(y_test)
+
+print(E_in, E_out)
+print('d')
 
 """
 Question 4
-We can calcuate the partial derivative mathematically.
+Rerun the same calculations with a different k
 
 """
 
 print('Question 4')
+k = 3
+
+inv_decayed = pd.DataFrame(np.linalg.pinv(xprimex.values + 10**k * np.identity(8)), xprimex.columns, xprimex.index)
+weights_decayed = inv_decayed.dot(X.transpose()).dot(y)
+
+E_in = np.sum(y != np.sign(weights_decayed.dot(X.transpose())))/len(y)
+E_out = np.sum(y_test != np.sign(weights_decayed.dot(X_test.transpose())))/len(y_test)
+print(E_in, E_out)
 print('e')
 
 """
 Question 5
 
-We need to iterate over the error surface. This can be done by taking the partial derivative and moving a step for both u and v.
-
+Loop over all possiblities
 """
 
 print('Question 5')
-eta = 0.1
-u, v = 1, 1
+
+for k in [-2, -1, 0, 1 ,2]:
+
+    inv_decayed = pd.DataFrame(np.linalg.pinv(xprimex.values + 10**k * np.identity(8)), xprimex.columns, xprimex.index)
+    weights_decayed = inv_decayed.dot(X.transpose()).dot(y)
+
+    E_in = np.sum(y != np.sign(weights_decayed.dot(X.transpose())))/len(y)
+    E_out = np.sum(y_test != np.sign(weights_decayed.dot(X_test.transpose())))/len(y_test)
+
+    print(k, E_in, E_out)
 
 
-for i in range(17):
-    if float((u*math.exp(v) - 2*v*math.exp(-u))**2) < 10**-14:
-        print("{} iterations".format(i))
-        break
-
-    u_diff = 2*(math.exp(v) + 2*v*math.exp(-u)) * (u*math.exp(v) - 2*v*math.exp(-u))
-    v_diff = 2*(u*math.exp(v) - 2*math.exp(-u)) * (u*math.exp(v) - 2*v*math.exp(-u))
-
-    u = u - eta * u_diff
-    v = v - eta * v_diff
-    
-
+print('d')
 
 """
 Question 6
-We can just print the final values after the gradient descent implemented earlier
+We can find this answer by observing the lowest out of sample error amongst integer k. We expect
+this to be bounded such that too large K causes an increase in out of sample error, so the 
+mininum should be a middle point.
 
 """
 
 print('Question 6')
-print((u, v))
+lowest = (10, 0)
+
+for k in range(-10, 10):
+
+    inv_decayed = pd.DataFrame(np.linalg.pinv(xprimex.values + 10**k * np.identity(8)), xprimex.columns, xprimex.index)
+    weights_decayed = inv_decayed.dot(X.transpose()).dot(y)
+
+    E_out = np.sum(y_test != np.sign(weights_decayed.dot(X_test.transpose())))/len(y_test)
+
+    if E_out < lowest[0]:
+        lowest = (E_out, k)
+
+print(lowest[0])
+print('b')
 
 """
 Question 7
-Using the same idea, we update alternatingly and then reevaluate before updating the other coordinate.
 
 """
 
 print('Question 7')
-eta = 0.1
-u, v = 1, 1
-
-
-for i in range(14):
-
-    #update alternatingly instead
-    u_diff = 2*(math.exp(v) + 2*v*math.exp(-u)) * (u*math.exp(v) - 2*v*math.exp(-u))
-    u = u - eta * u_diff
-    
-    v_diff = 2*(u*math.exp(v) - 2*math.exp(-u)) * (u*math.exp(v) - 2*v*math.exp(-u))
-    v = v - eta * v_diff
-    
-print((u*math.exp(v) - 2*v*math.exp(-u))**2)
+print('c')
 
 """
-Question 8 and 9
-
-
+Question 8
 
 """
-
-N = 100
-runs = 100
-eta = 0.01
-
-def setup_problem(N):
-    points = []
-    for i in range(N):
-        coord = [random.uniform(-1,1), random.uniform(-1,1)]
-        points.append(coord)
-        
-    points = np.array(points)
-    
-    # get bisecting line
-    point1 = (random.uniform(-1,1), random.uniform(-1,1))
-    point2 = (random.uniform(-1,1), random.uniform(-1,1))
-    a = (point1[1] - point2[1])/(point1[0] - point2[0])
-    b = point1[1] - a*point1[0]
-    
-    labels = []
-    for point in points:
-        if point[1] < a * point[0] + b:
-            labels.append(1)
-        else:
-            labels.append(-1)
-            
-    labels = np.array(labels)     
-    
-    equation = (a,b)
-    
-    return list(zip(points, labels)), equation
-
-#measure error on new data
-def measure_accuracy(f, g):
-    num = 1000
-    points = []
-    for i in range(num):
-        coord = [random.uniform(-1,1), random.uniform(-1,1)]
-        points.append(coord)
-        
-    points = np.array(points)
-    
-    #get true labels
-    f_labels = []
-    
-    for point in points:
-        if point[1] < f[0] * point[0] + f[1]:
-            f_labels.append(1)
-        else:
-            f_labels.append(-1)
-
-    g_labels = []
-    
-    for point in points:
-        point_data = np.hstack((np.array(1), point))
-        g_labels.append(np.matmul(g, np.transpose(point_data)))
-
-
-    f_labels = np.array(f_labels)
-    g_labels = np.array(g_labels)    
-    
-
-    #TODO: test error measure 
-    #correct error measure is not classification error, must implement cross entropy error
-    #return percent of error
-    return 1/num * sum(math.log1p(1 + math.exp(-f_labels * g_labels)))
-
-def log_reg(labelled_points):
-    
-    #logistic regression for 2d space
-    weights = [0, 0, 0]
-
-    nloops = 0
-    
-    while True:
-        
-        #store a copy of the old weights
-        old_weights = weights
-
-        #shuffle points randomly
-        random.shuffle(labelled_points)
-        
-        for point, label in labelled_points:
-
-            #add intercept term
-            point_data = np.hstack((np.array(1), point))
-
-            #use current weights to generate predicted value and gradient
-            hypothesis = np.matmul(weights, np.transpose(point_data))
-            gradient = -(label* point_data)/(1 + math.exp(label * hypothesis))
-
-            #update weights
-            weights = weights - eta * gradient
-
-        #increment loop counter
-        nloops += 1
-        
-        if np.linalg.norm(np.subtract(old_weights, weights)) < 0.01:
-            #return number of loops used
-            return weights, nloops
-
-        
-E_out = []
-Epochs = []
-
-for i in range(runs):
-    labelled_points, equation = setup_problem(N)
-    
-    #train logistic regression
-    weights, epochs = log_reg(labelled_points)
-    
-    E_out.append(measure_accuracy(equation, weights))
-    Epochs.append(epochs)
-
 
 print('Question 8')
-print(np.average(E_out))
+print('d')
+
+"""
+Question 9
+
+"""
 
 print('Question 9')
-print(np.average(Epochs))
+print('a')
 
 
 
 """
 Question 10
-
-The perceptron learning algorithm is unique in that it only cares about fixing any points that are mislabelled, but any points
-correctly labelled are effectively disregarded.
-The only error measure where this is properly accounted for is e.
 
 
 """
